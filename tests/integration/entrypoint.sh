dcf48d30da057292de8dccd779b51f4cf3c8ee14
Mikhail Yohman <mikhail.yohman@gmail.com>
2021-08-08 17:58:32 -0600

    Attempting to get testing working via Travis

--- tests/integration/entrypoint.sh
+++ tests/integration/entrypoint.sh
@@ -24,10 +24,10 @@ function main {
     echo "# Rendering integration configuration"
     render "./tests/integration/integration_config.tmpl.yml" > ./tests/integration/integration_config.yml
 
-    echo "Checking to make sure Nautobot server is reachable.."
+    echo "# Checking to make sure Nautobot server is reachable.."
     timeout 300 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' nautobot:8000)" != "200" ]]; do echo "waiting for Nautobot"; sleep 5; done' || false
 
-    echo "Populating Nautobot for running integration tests.."
+    echo "# Populating Nautobot for running integration tests.."
     python ./tests/integration/nautobot-populate.py
 
     echo "# Running..."
